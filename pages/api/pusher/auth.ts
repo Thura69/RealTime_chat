eb30242ab803753dbe29171ffb80d6e3faf6aa37
Thura69 <spyking64@gmail.com>
2024-01-15 23:10:11 +0630

    fix(typeError)

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,25 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { getServerSession } from "next-auth";
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 
 import { pusherServer } from "@/app/libs/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
 
 export default async function handler(
-  request: NextApiRequest, 
-  response: NextApiResponse
+    request: NextApiRequest,
+    response:NextApiResponse
 ) {
-  const session = await getServerSession(request, response, authOptions);
+    const session = await getServerSession(request, response, authOptions);
+    
+    if (!session?.user?.email) return response.status(401);
+    const socketId = request.body.socket_id;
+    const channel = request.body.channel_name;
 
-  if (!session?.user?.email) {
-    return response.status(401);
-  }
+    const data = {
+        user_id: session.user.email
+    };
 
-  const socketId = request.body.socket_id;
-  const channel = request.body.channel_name;
-  const data = {
-    user_id: session.user.email,
-  };
+    const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
 
-  const authResponse = pusherServer.authorizeChannel(socketId, channel, data);
-  return response.send(authResponse);
+    return response.send(authResponse);
 };
